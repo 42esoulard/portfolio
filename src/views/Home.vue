6750f01a5478e653b6d1e6dae4caaa75129bebe5
esoulard <esoulard@student.42.fr>
2021-12-29 13:36:36 +0100

    reduced blur reduced lag reduced pain

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,7 +3,7 @@
     
     <div class="home-neon-pretext">Hi, I'm</div>
     <div class="home-neon-wrapper">
-      <router-link  class="home-neon-text" :to="{ name: 'About'}">Estell<span class="home-neon-text--offset">e</span></router-link>
+      <router-link  class="home-neon-text" :to="{ name: 'About'}">Est<span class="home-neon-text--offset">el</span>l<span class="home-neon-text--offset">e</span></router-link>
     </div>
     <div class="home-subtitle"><router-link :to="{ name: 'Work'}" class="home-subtitle home-subtitle--dev">a &lt;dev&gt;</router-link>
     <router-link :to="{ name: 'Contact'}" class="home-hire-me-wrapper">
