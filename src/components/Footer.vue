eb6351ef11f8508f2f47b042e417f7aa816a5c8e
esoulard <esoulard@student.42.fr>
2021-12-29 18:57:13 +0100

    footer link

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="footer-container">
-    <div>Built by <a href="http://www.estellesoulard.com" class="footer-link" title="GitHub: 42esoulard">esoulard</a> - 2021</div>
+    <div>Built by <a href="https://www.estellesoulard.com/" class="footer-link" title="https://www.estellesoulard.com/">esoulard</a> - 2021</div>
   </div>
 </template>
 
